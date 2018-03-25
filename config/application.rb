30f9116e84c09ac9e111b461072db6f83bb1ea12
consti <consti@consti.de>
2018-03-25 21:35:34 +0700

    Enable Rack::Deflate

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Cblog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Enforce gzip compression where possible
+    config.middleware.use Rack::Deflater
   end
 end
